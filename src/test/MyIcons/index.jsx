300799b6e89e55a2a1397901aed4dc91b43edc0d
LiuXianjin <liuxianjin@pudutech.com>
2022-07-17 04:46:34 +0800

    feat: 显示CloseIcon等图标

--- src/test/MyIcons/index.jsx
+++ src/test/MyIcons/index.jsx
@@ -1,9 +1,22 @@
 import CircleCloseIcon from "../../components/Icons/CircleCloseIcon"
+import CloseIcon from "../../components/Icons/CloseIcon"
+import DownOutlined from "../../components/Icons/DownOutlined"
+import DownTriangle from "../../components/Icons/DownTriangleIcon"
+import RightOutlined from "../../components/Icons/LeftOutlined"
+import LeftOutlined from "../../components/Icons/RightOutlined"
+import RightTriangle from "../../components/Icons/RightTriangleIcon"
+import "./index.css"
 
 const MyIcons = () => {
 
     return <div className="my-icons">
         <CircleCloseIcon />
+        <CloseIcon />
+        <DownOutlined />
+        <DownTriangle />
+        <LeftOutlined />
+        <RightOutlined />
+        <RightTriangle />
     </div>
 }
 
